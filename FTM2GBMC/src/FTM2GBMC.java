5601380e66abe06ebc0c517ba51dcedfef53d621
Joseph El-Khouri <joseph.elkhouri@gmail.com>
2015-10-29 11:29:26 -0400

    Added text importer and macros to main class

--- FTM2GBMC/src/FTM2GBMC.java
+++ FTM2GBMC/src/FTM2GBMC.java
@@ -1,13 +1,45 @@
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class FTM2GBMC {
 
+    private final ArrayList<String> textImport;
+    
     // Song information
     private String songTitle;
     private String songAuthor;
     private String songCopyright;
     
-    public static void main(String[] args) throws Exception {
-        
+    // Macros
+    ArrayList<MacroVolume> volumeMacros;
+    ArrayList<MacroArp> arpeggioMacros;
+    ArrayList<MacroPitch> pitchMacros;
+    ArrayList<MacroDuty> dutyMacros;
+    
+    
+    
+    public FTM2GBMC(ArrayList<String> textImport) {
+        this.textImport = textImport;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("------------------------");
+        System.out.println("|      Welcome to      |");
+        System.out.println("| Savestate's FTM2GBMC |");
+        System.out.println("------------------------");
+        System.out.print("FamiTracker text export --> ");
+        String input = sc.nextLine();
+        input = input.replaceAll("\\\"", "");
+        Charset encoding = Charset.defaultCharset();
+        ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(Paths.get(input), encoding);
+        FTM2GBMC ftm2gbmc = new FTM2GBMC(lines);
     }
     
 }
